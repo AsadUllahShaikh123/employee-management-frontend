ef9207f775a9fe40dc7471fd67fad074b44ff021
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-10-30 01:16:37 +0500

    added App.js and index.css files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import HeaderComponent from './component/Header';
+import FooterComponent from './component/Footer';
+import  './index.css';
+import ListEmployeeComponent from './component/ListEmployee';
+import AddEmployeeComponent from './component/AddEmployee';
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <>
+      <HeaderComponent/>
+      <ListEmployeeComponent/>
+      <AddEmployeeComponent/>
+      <FooterComponent/>
+    </>
   )
 }
 
