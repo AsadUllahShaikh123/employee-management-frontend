ed876b49fde79d78615db24574adb297330b72b8
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-10-31 23:32:32 +0500

    Added all methods

--- src/service/EmployeeService.js
+++ src/service/EmployeeService.js
@@ -6,6 +6,18 @@ class EmployeeService{
   getAllEmployee(){
     return axios.get(BASE_URL);
   }
+  saveEmployee(employeeData){
+    return axios.post(BASE_URL,employeeData);
+  }
+  updateEmployee(id,employeeData){
+    return axios.put(`${BASE_URL}/${id}`,employeeData);
+  }
+  getEmployeeById(id){
+    return axios.get(`${BASE_URL}/${id}`)
+  }
+  deleteEmployee(id){
+    return axios.delete(`${BASE_URL}/${id}`)
+  }
 
 }
 export default new EmployeeService();
